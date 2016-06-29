1c824fce704593d2b616d4be893032775f1415ef
Simon Situ <ssitu001@gmail.com>
2016-06-29 00:43:39 -0700

    complete GCD

---
+++ GCD.js
@@ -0,0 +1,25 @@
+GCD.js
+
+// 2. Find the greatest common divisor (gcd) of two positive numbers.
+// The GCD of two integers x and y is the greatest integer that divides
+// both x and y with no remainder.
+// Example:  gcd(4,36);  // 4
+
+
+// facts
+// inputs 2 numbers
+// output one number which is the GCD
+
+
+const findGCD = (num1, num2) => {
+
+  let dividend = Math.max(num1, num2);
+  let divisor = Math.min(num1, num2);
+  let quotient = Math.floor(dividend/divisor);
+  let remainder = dividend % divisor;
+  
+  return remainder === 0 ? divisor : findGCD(remainder, divisor); 
+
+};
+
+findGCD(108, 30) //6
